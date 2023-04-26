efe9e3a15f05a583ca240ed3e9a1c79d3fb1aec2
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2023-04-25 23:48:35 -0500

    [add] - LifeCycle ViewController 🤩

--- lyfeCycleStoryboard/lyfeCycleStoryboard/ViewController.swift
+++ lyfeCycleStoryboard/lyfeCycleStoryboard/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    override func loadView() {
+        super.loadView()
+        print("loadView - el primero en ejecutarse mientras carga (solo una vez)")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("viewDidLoad - Carga solo una vez")
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        print("viewWillAppear - la vista va a aparecer")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        print("viewDidAppear - la vista ya aparecio ")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        print("viewWillDidsappear - la vista va a desaparecer")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(true)
+        print("viewDidDissapear la vista ya desaparecio ")
+    }
+    
+    
 }
 
