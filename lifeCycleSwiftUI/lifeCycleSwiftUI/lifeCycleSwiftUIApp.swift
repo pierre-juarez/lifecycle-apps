a815341d57b7ef4998bd95fd6c125804707bb81e
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2023-04-25 23:28:38 -0500

    [add] - Initial commit 🚀

---
+++ lifeCycleSwiftUI/lifeCycleSwiftUI/lifeCycleSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  lifeCycleSwiftUIApp.swift
+//  lifeCycleSwiftUI
+//
+//  Created by Pierre Juarez U. on 25/04/23.
+//
+
+import SwiftUI
+
+@main
+struct lifeCycleSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
