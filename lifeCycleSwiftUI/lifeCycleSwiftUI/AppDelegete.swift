0029a9bbfa8cde447e1a6eef635fc977b65cb3fa
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2023-04-26 00:04:37 -0500

    [add] - LifeCycle SwiftUI 🤩

---
+++ lifeCycleSwiftUI/lifeCycleSwiftUI/AppDelegete.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegete.swift
+//  lifeCycleSwiftUI
+//
+//  Created by Pierre Juarez U. on 25/04/23.
+//
+
+import UIKit
+
+class AppDelegate: NSObject , UIApplicationDelegate{
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("Hello from AppDelegate")
+        return true
+    }
+    
+}
--- lifeCycleSwiftUI/lifeCycleSwiftUI/lifeCycleSwiftUIApp.swift
+++ lifeCycleSwiftUI/lifeCycleSwiftUI/lifeCycleSwiftUIApp.swift
@@ -9,9 +9,33 @@ import SwiftUI
 
 @main
 struct lifeCycleSwiftUIApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @Environment(\.scenePhase) var scenePhase
+    
+    init(){
+        firstFunction()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }.onChange(of: scenePhase) { phase in
+            switch phase {
+            case .active:
+                print("It's active")
+            case .inactive:
+                print("It's inactive")
+            case .background:
+                print("It's in background")
+            @unknown default:
+                fatalError("Error")
+            }
         }
     }
+    
+    func firstFunction(){
+        print("First action...")
+    }
+    
 }
